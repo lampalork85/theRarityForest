136abd0be3baa8b6c2c22817d0908e1a9b3e8b91
lampalork <lampalork@gmail.com>
2022-05-14 16:27:45 +0200

    upgrade solidity version

--- contracts/theRarityForest.sol
+++ contracts/theRarityForest.sol
@@ -1,7 +1,7 @@
-//SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./interfaces/IRarity.sol";
 
